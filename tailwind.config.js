12b4e98fb80baae5b2e84d75517dca6d862489b1
zackdesu <zarchxxx@gmail.com>
2023-06-19 22:02:35 +0700

    extend breakpoints

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,6 +12,9 @@ module.exports = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      screens: {
+        menu: "1493px",
+      },
     },
     fontFamily: {
       poppins: ["Poppins", "sans-serif"],
