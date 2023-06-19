760a277e332e5b16292f59c5af4251101b952c60
zackdesu <zarchxxx@gmail.com>
2023-06-19 22:04:14 +0700

    margin menjadi padding navbar

--- components/navbar.jsx
+++ components/navbar.jsx
@@ -3,7 +3,7 @@ import { FiSearch } from "react-icons/fi";
 
 const Navbar = () => {
   return (
-    <header className="flex justify-between my-2 mx-8 p-5 border-b-2 backdrop-blur fixed top-0 left-0 right-0">
+    <header className="flex justify-between mx-8 py-8 px-5 border-b-2 backdrop-blur fixed top-0 left-0 right-0 z-50">
       <h1>Dimas Tech Supply</h1>
       <FiSearch size={30} />
     </header>
