91dc5f5f9b724c4e5871fe7e69da0682db61adfd
zackdesu <zarchxxx@gmail.com>
2023-06-19 22:03:01 +0700

    penyesuaian card

--- components/card.jsx
+++ components/card.jsx
@@ -5,10 +5,12 @@ const Card = () => {
   return (
     <div className="w-[192px] h-64 bg-[#d9d9d9] rounded p-2 mr-4 flex flex-col items-center text-center">
       <div className="w-[95%] h-[60%] bg-black"></div>
-      <p>Nama Produk</p>
-      <p className="flex items-center">
-        <AiFillStar /> | 5.0 Terjual 5
-      </p>
+      <div className="my-5">
+        <p className="font-semibold">Nama Produk</p>
+        <p className="flex items-center">
+          <AiFillStar className="text-yellow-500" /> | 5.0 Terjual 5
+        </p>
+      </div>
     </div>
   );
 };
