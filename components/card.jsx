7f5f85c8891441d92f3ff1efd9a798b24241488e
zackdesu <zarchxxx@gmail.com>
2023-06-21 14:16:33 +0700

    responsive card(need to change again)

--- components/card.jsx
+++ components/card.jsx
@@ -3,9 +3,9 @@ import { AiFillStar } from "react-icons/ai";
 
 const Card = () => {
   return (
-    <div className="w-[192px] h-64 bg-[#d9d9d9] rounded p-2 mr-4 flex flex-col items-center text-center">
+    <div className="max-sm:w-[96px] max-sm:h-[128px] max-lg:w-36 max-lg:h-48 w-48 h-64 bg-[#d9d9d9] rounded max-sm:p-1 p-2 mr-4 flex flex-col items-center text-center">
       <div className="w-[95%] h-[60%] bg-black"></div>
-      <div className="my-5">
+      <div className="max-sm:my-2 max-lg:my-3 my-5">
         <p className="font-semibold">Nama Produk</p>
         <p className="flex items-center">
           <AiFillStar className="text-yellow-500" /> | 5.0 Terjual 5
