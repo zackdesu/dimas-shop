2751faf9d54b1502d691092f525d0f6e4cb1b03d
zackdesu <zarchxxx@gmail.com>
2023-06-21 14:16:47 +0700

    responsive navbar

--- components/navbar.jsx
+++ components/navbar.jsx
@@ -4,18 +4,18 @@ import { FiSearch } from "react-icons/fi";
 
 const Navbar = () => {
   return (
-    <header className="flex justify-between mx-8 py-8 px-5 border-b-2 backdrop-blur fixed top-0 left-0 right-0 z-50">
+    <header className="flex justify-between max-sm:py-4 max-sm:px-3 py-8 px-5 border-b-2 backdrop-blur fixed top-0 left-0 right-0 z-50 items-center">
       <div className="flex items-center">
         <Image
           src={"/icon.png"}
           width={50}
           height={50}
           alt="Dimas Tech Supply"
-          className="mr-5"
+          className="mr-5 max-sm:w-[40px] max-sm:h-[40px]"
         />
-        <h1>Dimas Tech Supply</h1>
+        <h1 className="max-sm:hidden">Dimas Tech Supply</h1>
       </div>
-      <FiSearch size={30} />
+      <FiSearch size={30} className="max-sm:w-[22px]" />
     </header>
   );
 };
