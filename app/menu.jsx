9aa1dd2b2e91859a1435a1acfe1cad76c9c82c29
zackdesu <zarchxxx@gmail.com>
2023-06-21 14:16:02 +0700

    delete dimaspay from menu & responsive menu

--- app/menu.jsx
+++ app/menu.jsx
@@ -10,21 +10,19 @@ import { GrMemory } from "react-icons/gr";
 import { ImPower } from "react-icons/im";
 import { FaMouse } from "react-icons/fa";
 import { HiOutlineDesktopComputer } from "react-icons/hi";
-import DimasPay from "./dimaspay";
-
 const Menu = () => {
   const IconMenu = ({ icon: Icon, name }) => (
     <div className="flex flex-col items-center">
-      <div className="p-3 bg-zinc-100 border border-zinc-300 flex flex-col items-center cursor-pointer mx-5">
-        <Icon size={20} />
+      <div className="p-3 bg-zinc-100 border border-zinc-300 flex flex-col items-center cursor-pointer mx-5 my-3">
+        <Icon size={15} />
       </div>
       <p className="mt-2 cursor-pointer">{name}</p>
     </div>
   );
 
   return (
-    <div className="flex items-center flex-wrap justify-center max-menu:flex-row-reverse">
-      <div className="p-3 px-5 mx-32 my-8 bg-zinc-200 rounded shadow-md flex border border-zinc-100 justify-center">
+    <div className="flex items-center flex-wrap justify-center mx-auto">
+      <div className="p-3 px-5 my-8 bg-zinc-200 rounded shadow-md flex border border-zinc-100 justify-center flex-wrap max-[1036px]:w-[472px] max-[478px]:w-[365px]">
         <IconMenu icon={BsCpu} name="CPU" />
         <IconMenu icon={BsMotherboard} name="Motherboard" />
         <IconMenu icon={BsHdmi} name="VGA" />
@@ -34,9 +32,6 @@ const Menu = () => {
         <IconMenu icon={HiOutlineDesktopComputer} name="Monitor" />
         <IconMenu icon={FaMouse} name="Mouse" />
         <IconMenu icon={BsKeyboard} name="Keyboard" />
-      </div>
-      <div className="mx-32 mt-10 mb-5 menu:ms-auto">
-        <DimasPay />
       </div>
     </div>
   );
