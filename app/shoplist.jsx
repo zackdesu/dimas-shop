15cc39a29b16647d6910f4d1f953289e4c8249db
zackdesu <zarchxxx@gmail.com>
2023-06-21 20:28:44 +0700

    add more card

--- app/shoplist.jsx
+++ app/shoplist.jsx
@@ -4,10 +4,16 @@ import React from "react";
 const ShopList = () => {
   return (
     <div className="flex">
-      <div className="mt-10 mb-5">
+      <div className="mt-10 mb-5 max-w-full">
         <h2>Rekomendasi Untukmu</h2>
         <p className="text-[#00000090]">Karena kamu baru saja mencari Laptop</p>
-        <div className="mt-5 flex flex-wrap">
+        <div className="mt-5 flex flex-row overflow-x-auto wrapper">
+          <Card />
+          <Card />
+          <Card />
+          <Card />
+          <Card />
+          <Card />
           <Card />
           <Card />
           <Card />
