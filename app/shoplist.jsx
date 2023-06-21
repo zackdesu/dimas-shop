c6da7eb76e2a874d85964121d93f01ef9d08f75b
zackdesu <zarchxxx@gmail.com>
2023-06-21 14:16:22 +0700

    add flex wrap & delete margin x

--- app/shoplist.jsx
+++ app/shoplist.jsx
@@ -4,10 +4,10 @@ import React from "react";
 const ShopList = () => {
   return (
     <div className="flex">
-      <div className="mx-32 mt-10 mb-5">
+      <div className="mt-10 mb-5">
         <h2>Rekomendasi Untukmu</h2>
         <p className="text-[#00000090]">Karena kamu baru saja mencari Laptop</p>
-        <div className="mt-5 flex">
+        <div className="mt-5 flex flex-wrap">
           <Card />
           <Card />
           <Card />
