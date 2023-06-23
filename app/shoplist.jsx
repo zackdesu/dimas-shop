72bf32574cf71348c618083f78cdc67e9145c996
zackdesu <zarchxxx@gmail.com>
2023-06-23 18:14:38 +0700

    fix slash

--- app/shoplist.jsx
+++ app/shoplist.jsx
@@ -6,7 +6,7 @@ import { useEffect, useState } from "react";
 async function getItems() {
   const base_url =
     process.env.NODE_ENV === "production"
-      ? "https://dimas-shop.vercel.app/"
+      ? "https://dimas-shop.vercel.app"
       : "http://localhost:3000";
   const res = await fetch(base_url + "/api/getitems");
   if (!res.ok) {
