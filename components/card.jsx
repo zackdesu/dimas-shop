af77d7e83e1ee121d7b40bd1fa568a8efd5ebe98
zackdesu <zarchxxx@gmail.com>
2023-06-25 00:08:51 +0700

    remove first div from card

--- components/card.jsx
+++ components/card.jsx
@@ -13,21 +13,19 @@ const Card = ({ Nama, Rating, Terjual, Harga }) => {
   };
 
   return (
-    <div>
-      <div className="max-md:w-32 max-md:h-48 w-48 h-64 bg-[#d9d9d9] rounded max-sm:p-1 p-2 mr-4 flex flex-col items-center text-center">
-        <div className="w-[95%] h-[60%] bg-black"></div>
-        <div className="max-sm:my-2 max-md:my-3 my-5 flex flex-col items-center">
-          <p className="font-semibold text-xs md:text-base">
-            <TruncatedText text={Nama} />
-          </p>
-          <p className="flex items-center text-xs md:text-sm">
-            <AiFillStar className="text-yellow-500" /> {Rating} | Terjual{" "}
-            {Terjual}
-          </p>
-          <p className="mt-2">
-            <b>{Harga}</b>
-          </p>
-        </div>
+    <div className="max-md:w-32 max-md:h-48 w-48 h-64 bg-[#d9d9d9] rounded max-sm:p-1 p-2 mr-4 flex flex-col items-center text-center">
+      <div className="w-[95%] h-[60%] bg-black"></div>
+      <div className="max-sm:my-2 max-md:my-3 my-5 flex flex-col items-center">
+        <p className="font-semibold text-xs md:text-base">
+          <TruncatedText text={Nama} />
+        </p>
+        <p className="flex items-center text-xs md:text-sm">
+          <AiFillStar className="text-yellow-500" /> {Rating} | Terjual{" "}
+          {Terjual}
+        </p>
+        <p className="mt-2">
+          <b>{Harga}</b>
+        </p>
       </div>
     </div>
   );
