37df1968c90941be599b089f7e75360ecc9f7b33
zackdesu <zarchxxx@gmail.com>
2023-06-27 09:28:00 +0700

    Add link and href for each icons

--- app/menu.jsx
+++ app/menu.jsx
@@ -10,28 +10,31 @@ import { GrMemory } from "react-icons/gr";
 import { ImPower } from "react-icons/im";
 import { FaMouse } from "react-icons/fa";
 import { HiOutlineDesktopComputer } from "react-icons/hi";
+import Link from 'next/link'
 const Menu = () => {
-  const IconMenu = ({ icon: Icon, name }) => (
+  const IconMenu = ({ icon: Icon, name, href }) => (
+  <Link href={`/${href}`}>
     <div className="flex flex-col items-center">
       <div className="p-3 bg-zinc-100 border border-zinc-300 flex flex-col items-center cursor-pointer mx-5 my-3">
         <Icon size={15} />
       </div>
       <p className="mt-2 cursor-pointer">{name}</p>
     </div>
+   </Link>
   );
 
   return (
     <div className="flex items-center flex-wrap justify-center mx-auto">
       <div className="p-3 px-5 my-8 bg-zinc-200 rounded shadow-md flex border border-zinc-100 justify-center flex-wrap max-[1036px]:w-[472px] max-[478px]:w-[365px]">
-        <IconMenu icon={BsCpu} name="CPU" />
-        <IconMenu icon={BsMotherboard} name="Motherboard" />
-        <IconMenu icon={BsHdmi} name="VGA" />
-        <IconMenu icon={BsMemory} name="RAM" />
-        <IconMenu icon={GrMemory} name="HDD/SSD" />
-        <IconMenu icon={ImPower} name="PSU" />
-        <IconMenu icon={HiOutlineDesktopComputer} name="Monitor" />
-        <IconMenu icon={FaMouse} name="Mouse" />
-        <IconMenu icon={BsKeyboard} name="Keyboard" />
+        <IconMenu icon={BsCpu} name="CPU" href="cpu" />
+        <IconMenu icon={BsMotherboard} name="Motherboard" href="motherboard" />
+        <IconMenu icon={BsHdmi} name="VGA" href="vga" />
+        <IconMenu icon={BsMemory} name="RAM" href="ram" />
+        <IconMenu icon={GrMemory} name="HDD/SSD" href="memory" />
+        <IconMenu icon={ImPower} name="PSU" href="psu" />
+        <IconMenu icon={HiOutlineDesktopComputer} name="Monitor" href="monitor" />
+        <IconMenu icon={FaMouse} name="Mouse" href="mouse" />
+        <IconMenu icon={BsKeyboard} name="Keyboard" href="keyboard" />
       </div>
     </div>
   );
