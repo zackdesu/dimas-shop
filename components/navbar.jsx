d600d270106777bf294eb0a966b39cfa53473888
zackdesu <zarchxxx@gmail.com>
2023-06-27 09:28:13 +0700

    Resize navbar & icons

--- components/navbar.jsx
+++ components/navbar.jsx
@@ -4,22 +4,23 @@ import { BsPersonFill } from "react-icons/bs";
 import { FiSearch } from "react-icons/fi";
 
 const Navbar = () => {
+  
   return (
-    <header className="flex justify-between max-sm:py-4 max-sm:px-3 py-8 px-5 border-b-2 backdrop-blur fixed top-0 left-0 right-0 z-50 items-center">
+    <header className="flex justify-between max-sm:py-4 max-sm:px-3 py-6 px-5 border-b-2 bg-zinc-100 fixed top-0 left-0 right-0 z-50 items-center">
       <div className="flex items-center">
         <Image
           src={"/icon.png"}
-          width={50}
-          height={50}
+          width={40}
+          height={40}
           alt="Dimas Tech Supply"
-          className="mr-5 max-sm:w-[40px] max-sm:h-[40px]"
+          className="mr-5 max-sm:w-[30px] max-sm:h-[30px]"
         />
         <h1 className="max-sm:hidden">Dimas Tech Supply</h1>
       </div>
       <div className="flex items-center">
         <BsPersonFill
-          size={40}
-          className="bg-neutral-200 rounded-full p-2 mx-2 max-sm:w-[30px] max-sm:h-[30px] cursor-pointer"
+          size={30}
+          className="mx-2 max-sm:w-[22px] max-sm:h-[22px] cursor-pointer"
         />
         <FiSearch size={30} className="max-sm:w-[22px] mx-2 cursor-pointer" />
       </div>
