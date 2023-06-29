0b1961e4475c868162c7ee6d39a48928835c750f
zackdesu <zarchxxx@gmail.com>
2023-06-29 08:35:33 +0700

    Full responsive for all device

--- app/product/[id]/page.js
+++ app/product/[id]/page.js
@@ -38,10 +38,11 @@ const Page = ({ params }) => {
   }, [harga, value]);
 
   return (
+  <>
       <div className="flex max-lg:flex-col justify-between my-5">
       <div className="flex max-sm:flex-col justify-between">
-        <div className="w-[250px] h-[250px] max-md:w-[200px] max-md:h-[200px] max-sm:w-full max-sm:h-[200px] bg-black"></div>
-        <div className="lg:hidden">
+        <div className="w-[250px] h-[250px] max-md:w-[200px] max-md:h-[200px] max-sm:w-full max-sm:h-[200px] bg-black shadow"></div>
+        <div className="lg:hidden mt-3">
         <h1>{data.Nama}</h1>
           <p className="flex items-center mb-10 max-sm:mb-4">
             <AiFillStar className="text-yellow-500" /> <b>{data.Rating}</b> &nbsp;|&nbsp; <b>Terjual</b> &nbsp;
@@ -54,26 +55,43 @@ const Page = ({ params }) => {
             }).format(data.Harga)}
           </h1>
           </div>
+          
         </div>
-          <div className="mx-2 max-sm:my-5 lg:hidden">
-          <div className="bg-zinc-300 my-10 rounded flex justify-between py-2 px-5">
+          <div className="mx-2 max-sm:my-5 lg:hidden sm:flex items-center justify-between">
+          <div className="bg-zinc-300 my-5 rounded flex justify-between py-2 px-5 sm:w-3/4">
             {new Intl.NumberFormat("id-ID", {
               style: "currency",
               currency: "IDR",
             }).format(hasil)}
+            <div>
+        <button className="border border-black rounded px-2" onClick={
+            () => {
+                if (value <= 0) setValue(0);
+              else setValue(value - 1); 
+              console.log(value)}
+            }>-</button>
             <input
               type="number"
-              className="w-[40px]"
+              className="w-[40px] mx-2 text-center"
+              value={value}
               onChange={(e) => {
-                if (e.target.value <= 0) e.target.value = null;
+                if (e.target.value < 0) e.target.value = null;
                 if (e.target.value >= data.Jumlah) e.target.value = data.Jumlah;
-                setValue(e.target.value);
+                setValue(parseInt(e.target.value));
               }}
             />
+          <button className="border border-black rounded px-2" onClick={() => {
+              if (value >= data.Jumlah) setValue(data.Jumlah);
+              else setValue(value + 1); 
+              console.log(value)}
+              }>+</button>
+              </div>
           </div>
-          <button className="bg-zinc-600 text-zinc-100 w-full font-semibold max-sm:fixed max-sm:bottom-2 max-sm:left-2 max-sm:right-2">Buy</button>
+          <div>
+          <button className="bg-zinc-600 text-zinc-100 font-semibold max-sm:hidden px-5 py-2 shadow">Buy</button>
+          </div>
         </div>
-        <div className="xl:w-6/12 lg:w-4/12 lg:overflow-y-auto lg:max-h-[80vh] p-1 flex flex-col">
+        <div className="min-[1440px]:w-6/12 xl:w-5/12 lg:w-4/12 lg:overflow-y-auto lg:max-h-[80vh] p-1 flex flex-col">
           <div className="max-lg:hidden">
         <h1>{data.Nama}</h1>
           <p className="flex items-center mb-10 max-sm:mb-4">
@@ -87,7 +105,7 @@ const Page = ({ params }) => {
             }).format(data.Harga)}
           </h1>
           </div>
-      <h2 className="mt-20 mb-3">Detail</h2>
+      <h2 className="mt-20 mb-3 max-sm:mt-10">Detail</h2>
       <table className="text-left max-lg:w-1/2 max-sm:w-full">
         <tbody>
           <Table nama="Kondisi" input={data.Kondisi} />
@@ -96,7 +114,7 @@ const Page = ({ params }) => {
           <Table nama="Rating" input={data.Rating} />
         </tbody>
       </table>
-      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Amet nisl purus in mollis nunc. Ac orci phasellus egestas tellus rutrum tellus pellentesque eu. Imperdiet massa tincidunt nunc pulvinar. Condimentum id venenatis a condimentum. Fermentum iaculis eu non diam phasellus vestibulum lorem sed risus. Pretium lectus quam id leo in vitae turpis massa. Pellentesque habitant morbi tristique senectus et netus. Sed faucibus turpis in eu mi bibendum. At varius vel pharetra vel turpis nunc eget. Ut etiam sit amet nisl purus in mollis nunc. Placerat vestibulum lectus mauris ultrices eros. Scelerisque eleifend donec pretium vulputate sapien nec. Ornare arcu dui vivamus arcu felis bibendum ut tristique et.
+      <p className="max-sm:mb-10">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Amet nisl purus in mollis nunc. Ac orci phasellus egestas tellus rutrum tellus pellentesque eu. Imperdiet massa tincidunt nunc pulvinar. Condimentum id venenatis a condimentum. Fermentum iaculis eu non diam phasellus vestibulum lorem sed risus. Pretium lectus quam id leo in vitae turpis massa. Pellentesque habitant morbi tristique senectus et netus. Sed faucibus turpis in eu mi bibendum. At varius vel pharetra vel turpis nunc eget. Ut etiam sit amet nisl purus in mollis nunc. Placerat vestibulum lectus mauris ultrices eros. Scelerisque eleifend donec pretium vulputate sapien nec. Ornare arcu dui vivamus arcu felis bibendum ut tristique et.
 
 Sapien et ligula ullamcorper malesuada. Arcu cursus vitae congue mauris rhoncus aenean vel elit scelerisque. Libero justo laoreet sit amet cursus sit amet dictum sit. Sagittis orci a scelerisque purus semper eget duis. Nam libero justo laoreet sit amet cursus sit. Feugiat vivamus at augue eget arcu dictum. Ac odio tempor orci dapibus ultrices in iaculis nunc sed. Urna neque viverra justo nec ultrices dui sapien eget mi. Malesuada nunc vel risus commodo viverra maecenas accumsan. Nibh sit amet commodo nulla facilisi.
 
@@ -117,7 +135,7 @@ Vestibulum sed arcu non odio euismod. Odio aenean sed adipiscing diam donec adip
             }>-</button>
             <input
               type="number"
-              className="w-[40px] mx-2"
+              className="w-[40px] mx-2 text-center"
               value={value}
               onChange={(e) => {
                 if (e.target.value < 0) e.target.value = null;
@@ -137,10 +155,12 @@ Vestibulum sed arcu non odio euismod. Odio aenean sed adipiscing diam donec adip
               currency: "IDR",
             }).format(hasil)}
           </div>
-          <button className="bg-zinc-600 text-zinc-100 w-full font-semibold max-sm:fixed max-sm:bottom-2 max-sm:left-2 max-sm:right-2 p-2">Buy</button>
+          <button className="bg-zinc-600 text-zinc-100 w-full font-semibold max-sm:fixed max-sm:bottom-2 max-sm:left-2 max-sm:right-2 p-2 shadow">Buy</button>
         </div>
         </div>
       </div>
+      <button className="bg-zinc-600 text-zinc-100 font-semibold sm:hidden fixed bottom-2 left-5 right-5 p-2 m-0 rounded shadow">Buy</button>
+      </>
   );
 };
 
