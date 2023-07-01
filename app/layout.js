a3e65dc09a7435388a096960db9693dae9af8986
zackdesu <zarchxxx@gmail.com>
2023-07-01 16:20:50 +0700

    SessionProvider in layout

--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,6 @@
 import Navbar from "@/components/navbar";
 import "./globals.css";
+import Provider from "./context/AuthContext";
 
 export const metadata = {
   title: "Dimas Tech Supply",
@@ -10,8 +11,10 @@ export default function RootLayout({ children }) {
   return (
     <html lang="id">
       <body className="box-border font-poppins">
+	<Provider>
         <Navbar />
         <div className="mt-[86px] max-sm:mt-[63px]">{children}</div>
+	</Provider>
       </body>
     </html>
   );
