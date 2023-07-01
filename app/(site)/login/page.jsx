e2e30fa438e959202e01021fb432e2bdc8430e01
zackdesu <zarchxxx@gmail.com>
2023-07-01 16:22:08 +0700

    login page

---
+++ app/(site)/login/page.jsx
@@ -0,0 +1,35 @@
+'use client'
+
+import React, {useEffect} from 'react'
+import { signIn } from 'next-auth/react'
+
+const InputForm = ({nama, type="text", className, ...props}) => (
+        <div className="flex flex-col mt-10 w-1/2">
+                <label htmlFor={nama} className="font-semibold text-zinc-600">{nama}</label>
+                <input type={type} id={nama} name={nama} autoComplete="off" className={"px-3 py-2 outline-none focus:ring-2 ring-cyan-500 "+ className} {...props}/>
+        </div>
+)
+
+export default function Page() {
+
+	const onSubmit = async (e) => {
+		e.preventDefault();
+		const data = {
+			Nama: e.target.Username.value,
+			Password: e.target.Password.value,
+			redirect: false
+			}
+		const res = await signIn('credentials', data)
+
+		console.log(res)
+				
+	}
+        return (
+                <form onSubmit={onSubmit} className="flex flex-col items-center mt-5">
+                        <h1>Login Page</h1>
+                        <InputForm nama="Username" required/>
+                        <InputForm nama="Password" type="password" className="invalid:ring-pink-500 invalid:text-pink-500" required minLength={6} maxLength={20}/>
+                        <button type="submit" className="py-2 px-5 bg-neutral-700 text-neutral-100 mt-5 rounded shadow">Login</button>
+                </form>
+        )
+}
