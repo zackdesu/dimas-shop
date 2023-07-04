bb272d77f42ac5e8a38a2796a552ac6abf6b946f
zackdesu <zarchxxx@gmail.com>
2023-07-04 16:49:39 +0700

    fix prisma bug & make the menu close when click the windows

--- app/page.js
+++ app/page.js
@@ -1,6 +1,4 @@
-'use client'
-
-import React, { useRef } from "react";
+import React from "react";
 import Carousel from "./carousel";
 import ShopList from "./shoplist";
 import Menu from "./menu";
@@ -8,13 +6,10 @@ import DimasPay from "./dimaspay";
 import Navbar from "@/components/navbar"
 
 const Page = () => {
-  const menu = useRef()
   return (
     <>
-      <Navbar ref={menu}/>
-      <div className="mt-[86px] max-sm:mt-[63px]" onClick={() => {
-	menu.current.classList.add('hidden')
-	}}>
+      <Navbar/>
+      <div className="mt-[86px] max-sm:mt-[63px]">
       <Carousel />
       <DimasPay />
       <Menu />
--- components/navbar.jsx
+++ components/navbar.jsx
@@ -1,15 +1,30 @@
 'use client'
 
 import Image from "next/image";
-import React, { forwardRef, useRef } from "react";
+import React, { forwardRef, useRef, useEffect, useState } from "react";
 import { BsPersonFill } from "react-icons/bs";
 import { FiSearch } from "react-icons/fi";
 import { signIn } from 'next-auth/react'
 import Link from 'next/link'
 
-const Navbar = forwardRef((props, ref) => {
+const Navbar = () => {
+  const menuRef = useRef(null)
+  const personRef = useRef(null)
+  const [isOpen, setIsOpen] = useState(false)
+  useEffect(() => {
+  	const click = (event) => {
+  		if(isOpen){
+  			setIsOpen(false)
+  		}
+  	}
+  	document.addEventListener('click', click)
+  	return () => {
+  	document.removeEventListener('click', click)
+  	}
+  }, [isOpen, setIsOpen])
+
   return (
-    <header className="flex justify-between max-sm:py-4 max-sm:px-3 py-6 px-5 border-b-2 bg-zinc-100 fixed top-0 left-0 right-0 z-50 items-center" onClick={() => ref.current.classList.add('hidden')}>
+    <header className="flex justify-between max-sm:py-4 max-sm:px-3 py-6 px-5 border-b-2 bg-zinc-100 fixed top-0 left-0 right-0 z-50 items-center">
       <div className="flex items-center">
         <Image
           src={"/icon.png"}
@@ -24,11 +39,10 @@ const Navbar = forwardRef((props, ref) => {
         <BsPersonFill
           size={30}
           className="mx-2 max-sm:w-[22px] max-sm:h-[22px] cursor-pointer"
-	  onClick={(e) => {e.stopPropagation();
-			  ref.current.classList.toggle('hidden')}}
+	  onClick={(e) => {e.stopPropagation(); isOpen ? setIsOpen(false) : setIsOpen(true)}}
         />
         <FiSearch size={30} className="max-sm:w-[22px] mx-2 cursor-pointer"/>
-      <nav className='bg-white absolute top-full p-2 rounded-sm hidden' ref={ref}>
+      <nav className={`bg-white absolute top-full p-2 rounded-sm ${isOpen ? '' : 'hidden'}`} ref={menuRef}>
 	<p onClick={() => signIn()} className="font-medium mb-2 cursor-pointer">Login</p>
 	<hr/>
 	<Link href="/register"><p className="font-medium mt-2 cursor-pointer">Sign Up</p></Link>
@@ -36,6 +50,6 @@ const Navbar = forwardRef((props, ref) => {
       </div>
     </header>
   );
-})
+}
 
 export default Navbar;
