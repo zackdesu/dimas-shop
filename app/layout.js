8cfadf4a594e6a433449915bdef95465fd298b13
zackdesu <zarchxxx@gmail.com>
2023-07-04 17:00:34 +0700

    Revert changes

--- app/layout.js
+++ app/layout.js
@@ -12,7 +12,25 @@ export default function RootLayout({ children }) {
     <html lang="id">
       <body className="box-border font-poppins">
 	<Provider>
-        {children}
+	  import Navbar from "@/components/navbar";
+import "./globals.css";
+import Provider from "./context/AuthContext";
+
+export const metadata = {
+  title: "Dimas Tech Supply",
+  description: "Dimas Tech Supply is a shop that sells PC components.",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="id">
+      <body className="box-border font-poppins">
+	<Provider>
+        <Navbar />
+        <div className="mt-[86px] max-sm:mt-[63px]">{children}</div>
+	</Provider>
+      </body>
+    </html>
 	</Provider>
       </body>
     </html>
--- app/page.js
+++ app/page.js
@@ -3,18 +3,14 @@ import Carousel from "./carousel";
 import ShopList from "./shoplist";
 import Menu from "./menu";
 import DimasPay from "./dimaspay";
-import Navbar from "@/components/navbar"
 
 const Page = () => {
   return (
     <>
-      <Navbar/>
-      <div className="mt-[86px] max-sm:mt-[63px]">
       <Carousel />
       <DimasPay />
       <Menu />
       <ShopList />
-      </div>
     </>
   );
 };
