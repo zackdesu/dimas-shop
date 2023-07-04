657b24ae1439d9fc5abd0ebe425075a83cf8ebac
zackdesu <zarchxxx@gmail.com>
2023-07-04 15:06:32 +0700

    reference for menu setting

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,8 @@
+'use client'
 import Navbar from "@/components/navbar";
 import "./globals.css";
 import Provider from "./context/AuthContext";
+import React, { useRef } from 'react'
 
 export const metadata = {
   title: "Dimas Tech Supply",
@@ -8,12 +10,15 @@ export const metadata = {
 };
 
 export default function RootLayout({ children }) {
+	const menu = useRef()
   return (
     <html lang="id">
       <body className="box-border font-poppins">
 	<Provider>
-        <Navbar />
-        <div className="mt-[86px] max-sm:mt-[63px]">{children}</div>
+        <Navbar ref={menu}/>
+        <div className="mt-[86px] max-sm:mt-[63px]" onClick={() => {
+	menu.current.classList.add('hidden')
+	}}>{children}</div>
 	</Provider>
       </body>
     </html>
