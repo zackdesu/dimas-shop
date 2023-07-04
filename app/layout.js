95aaef1967f6b49877b1dbf2a7dfd5d23a004b84
zackdesu <121839025+zackdesu@users.noreply.github.com>
2023-07-04 15:17:21 +0700

    Remove use client from layout and remove div layout

--- app/layout.js
+++ app/layout.js
@@ -1,8 +1,7 @@
-'use client'
 import Navbar from "@/components/navbar";
 import "./globals.css";
 import Provider from "./context/AuthContext";
-import React, { useRef } from 'react'
+import React from 'react'
 
 export const metadata = {
   title: "Dimas Tech Supply",
@@ -10,15 +9,11 @@ export const metadata = {
 };
 
 export default function RootLayout({ children }) {
-	const menu = useRef()
   return (
     <html lang="id">
       <body className="box-border font-poppins">
 	<Provider>
-        <Navbar ref={menu}/>
-        <div className="mt-[86px] max-sm:mt-[63px]" onClick={() => {
-	menu.current.classList.add('hidden')
-	}}>{children}</div>
+        {children}
 	</Provider>
       </body>
     </html>
