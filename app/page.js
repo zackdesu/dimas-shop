29b9ec5ffa9c5d4302ef4ecb3aa54e2c99c3054a
zackdesu <121839025+zackdesu@users.noreply.github.com>
2023-07-04 15:21:26 +0700

    Update page.js to use client and move the hide function to here

--- app/page.js
+++ app/page.js
@@ -1,16 +1,25 @@
-import React from "react";
+'use client'
+
+import React, { useRef } from "react";
 import Carousel from "./carousel";
 import ShopList from "./shoplist";
 import Menu from "./menu";
 import DimasPay from "./dimaspay";
+import Navbar from "@/components/navbar"
 
 const Page = () => {
+  const menu = useRef()
   return (
     <>
+      <Navbar ref={menu}/>
+      <div className="mt-[86px] max-sm:mt-[63px]" onClick={() => {
+	menu.current.classList.add('hidden')
+	}}>
       <Carousel />
       <DimasPay />
       <Menu />
       <ShopList />
+      </div>
     </>
   );
 };
